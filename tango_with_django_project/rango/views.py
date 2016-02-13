abfe1a442027aba7d349ced23102a91effbb462e
nathan_jeff_huang <nathan_huang@foxmail.com>
2016-02-14 00:40:57 +0800

    rango Signed-off-by: nathan_jeff_huang <nathan_huang@foxmail.com>

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Rango say hey there world!")
+    context_dict = {'boldmessage':"I am bold from the context"}
+    return render(request, 'rango/index.html', context_dict)
+
+def about(request):
+    return HttpResponse("<a href=/rango/>I'm about</a>")
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -82,3 +82,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+
+TEMPLATE_DIRS = [
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+]
