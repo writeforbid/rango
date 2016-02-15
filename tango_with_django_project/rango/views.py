4a866b39648f76469c340b1ff0857ad724b998ae
nathan_jeff_huang <nathan_huang@foxmail.com>
2016-02-15 23:53:51 +0800

    for strike??? Signed-off-by: nathan_jeff_huang <nathan_huang@foxmail.com>

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -10,4 +10,5 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("<a href=/rango/>I'm about</a>")
+    context_dict = {'aboutmessage':"I am from aboutmessage"}
+    return render(request,'rango/about.html', context_dict)
